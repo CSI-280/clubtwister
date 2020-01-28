0f2a231285ee7b9f0ed49c3d32b220297fff837b
Jared Remsberg <jaredremsberg@gmail.com>
2020-01-27 22:17:11 -0500

    This dont work

--- scripts.js
+++ scripts.js
@@ -15,11 +15,10 @@ function getType(animalType) {
 
 			for (var i = 0; i < numAnimals; i++) {
 				var currentSlot = document.getElementById(i);
-				currentSlot.innerHTML = "<td>" + animals[i].name + "</td>";
-				currentSlot.innerHTML += "<td>" + animals[i].breeds.primary + "</td>"
-				currentSlot.innerHTML += "<td>" + animals[i].age + "</td>";
-				currentSlot.innerHTML += "<td>" + animals[i].contact.address.city + ", " + animals[i].contact.address.state + "</td>"
-
+				currentSlot.innerHTML = `<td><button id='pfbtn' onclick="showInfo(${animals[i].id}, ${i})">${animals[i].name}</button></td>`;
+				currentSlot.innerHTML += `<td>${animals[i].breeds.primary}</td>`;
+				currentSlot.innerHTML += `<td>${animals[i].age}</td>`;
+				currentSlot.innerHTML += `<td>${animals[i].contact.address.city}, ${animals[i].contact.address.state}</td>`;
 			}
 
 			console.log(animals[0]);
@@ -31,3 +30,14 @@ function getType(animalType) {
 		});
 
 }
+
+// function showInfo(petID, row){
+// 	var profile = document.getElementsByClassName(`pf ${row}`);
+// 	pf.animal.search({id: petID}).then(
+// 		function (response){
+// 			let pet = response.data.animals[0];
+// 			console.log(pet)
+// 			profile[0].style.display = 'table-row'
+// 		}
+// 	)
+// }
